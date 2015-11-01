9db195e84a439d4e5f65fd429980e9a3f1f50092
Bradley Golden <bradleygolden@Bradleys-MacBook-Pro.local>
2015-11-01 12:58:51 -0600

    update

--- ScaledPoint.java
+++ ScaledPoint.java
@@ -1,12 +1,36 @@
 public class ScaledPoint
 {
+    private float x;
+    private float y;
+    private static int height = 0;
+    private static int width = 0;
+
     public ScaledPoint()
     {
-
+        x = 0.0;
+        y = 0.0;
     }
 
-    public static void main(String[] args)
+    public void updateWindow(int h, int w) {
+        height = h;
+        width = w;
+    }
+
+    public int getX() {
+        return x * width;
+    }
+
+    public int getY() {
+        return y * height;
+    }
+
+    public void setX(float x)
     {
-        // Test Code Here
+        this.x = x;
+    }
+
+    public void setY(float y)
+    {
+        this.y = y;
     }
 }
