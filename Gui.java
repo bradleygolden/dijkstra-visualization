8c521b628c583f01fb7fc6bf0b6325944a63fd40
MaciejSzpakowski <systemzajety@yahoo.com>
2015-11-04 00:25:23 -0600

    Added: timer, double buffering

--- Gui.java
+++ Gui.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-class AFrame extends JFrame implements MouseListener
+class AFrame extends JFrame implements MouseListener, ActionListener
 {
+    private Image backBuffer;
+    public static final int TIMER_CYCLE = 25; // period between redrawTimer triggers in ms
+    private Timer redrawTimer;     // timer that will repeatedly redraw the context if necessary
 	private ScaledPoint draggedPt; // point that is being dragged
-    private String mouseState;     // state of paint
     private int radius;            // node radius
 	
 	Graph graph;
@@ -19,8 +24,10 @@ class AFrame extends JFrame implements MouseListener
         setSize( 800 , 600 );
         setVisible( true );
         addMouseListener(this);
+        redrawTimer = new Timer(TIMER_CYCLE,null);
+        redrawTimer.addActionListener(this);
+        backBuffer = new BufferedImage(1920, 1080, BufferedImage.TYPE_INT_RGB);
         
-        mouseState = "up";
         radius = 15;
         draggedPt = null;
         
@@ -29,33 +36,31 @@ class AFrame extends JFrame implements MouseListener
     
     @Override
     public void paint(Graphics g)
-    {
-    	ScaledPoint.updateWindow(getHeight(), getWidth());
-    	
-    	if(draggedPt != null) // drag point if there is one to drag
-        {
-            dragPoint();
-        }
-    	
-    	super.paint(g);
-    	for(Node n : graph.nodes)
-    		g.drawOval(n.getScaledPoint().getX()-radius, n.getScaledPoint().getY()-radius, 2*radius, 2*radius);
-    	for(Edge e : graph.getEdges())
-    		g.drawLine(e.getStart().getScaledPoint().getX(),e.getStart().getScaledPoint().getY(),
-    				e.getEnd().getScaledPoint().getX(),e.getEnd().getScaledPoint().getY());
+    {    	
+        ScaledPoint.updateWindow(getHeight(), getWidth());
+
+        //if(backBuffer.getHeight(null) != getHeight() || backBuffer.getWidth(null) != getWidth())
+        //	backBuffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        
+        Graphics frameg = g;
+        if(backBuffer == null) // weird java
+        	return;
+        g = backBuffer.getGraphics();
+        g.setColor(Color.BLACK);
+        
+        for(Node n : graph.nodes)
+            g.drawOval(n.getScaledPoint().getX()-radius, n.getScaledPoint().getY()-radius, 2*radius, 2*radius);
+        for(Edge e : graph.getEdges())
+            g.drawLine(e.getStart().getScaledPoint().getX(),e.getStart().getScaledPoint().getY(),
+                       e.getEnd().getScaledPoint().getX(),e.getEnd().getScaledPoint().getY());
+        frameg.drawImage(backBuffer, 0, 0, null);
+        super.paint(g);
     }
     
     void dragPoint()
-    {
-        if(mouseState.equals("up"))
-        {
-            draggedPt = null;
-            return;
-        }
-        
+    {        
         draggedPt.setX((double)getMousePosition().x/getWidth());
         draggedPt.setY((double)getMousePosition().y/getHeight());
-        repaint();
     }
     
     int getDistanceFromMouse(ScaledPoint p, int mouseX, int mouseY)
@@ -65,14 +70,13 @@ class AFrame extends JFrame implements MouseListener
     
     public void mousePressed(MouseEvent e)
     {
-        mouseState = "down";
-        
         // find first point that collides with mouse
         for(Node n : graph.getNodes())
         {
             if(getDistanceFromMouse(n.getScaledPoint(), e.getX(), e.getY()) < radius)
             {
                 draggedPt = n.getScaledPoint();
+                redrawTimer.start();
                 dragPoint();
                 break;
             }
@@ -81,7 +85,7 @@ class AFrame extends JFrame implements MouseListener
 
     public void mouseReleased(MouseEvent e)
     {
-        mouseState = "up";
+    	draggedPt = null;
     }
     
     @Override
@@ -100,6 +104,23 @@ class AFrame extends JFrame implements MouseListener
     public void mouseExited(MouseEvent arg0)
     {
         draggedPt = null;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == redrawTimer)
+        {
+            if(draggedPt != null) // drag point if there is one to drag
+            {
+                dragPoint();
+                repaint();
+            }
+            else
+            {
+                redrawTimer.stop();
+            }
+        }
     }
 }
 
