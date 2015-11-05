50f6589e5a4ecfeb2185d68cf937ba50102fca7a
Amanda Olson <aolson7@uic.edu>
2015-11-05 08:12:30 -0600

    Added constructor to EdgeBE

---
+++ src/BusinessTier/GraphBE.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessTier;
+
+/**
+ *
+ * @author Amanda Olson
+ */
+public class GraphBE {
+    
+}
