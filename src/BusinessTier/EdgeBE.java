67d603ebfa23f7ba454d2563ff6b4ba1aaffd83d
Amanda Olson <aolson7@uic.edu>
2015-11-04 23:49:34 -0600

    Added constructor to EdgeBE

--- src/BusinessTier/EdgeBE.java
+++ src/BusinessTier/EdgeBE.java
@@ -9,6 +9,24 @@ package BusinessTier;
  *
  * @author Amanda Olson
  */
-public class EdgeBE {
+public class EdgeBE
+{
+    int    edgeWeight;     // path cost of edge
+    NodeBE edgeStart;      // starting point of path
+    NodeBE edgeEnd;        // destination node of path
     
+    /**
+    * Creates an EdgeBE object with edgeWeight a valid positive integer
+    *     start and end nodes on the valid set of possible nodes
+    * @param weight
+    * @param start
+    * @param end
+    */
+    public EdgeBE(int weight, NodeBE start, NodeBE end)
+    {
+        this.edgeWeight   = weight;
+        this.edgeStart = start;
+        this.edgeEnd = end;
+    }
+
 }
